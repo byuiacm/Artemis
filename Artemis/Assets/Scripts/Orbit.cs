7222c2b4f9b79528d37efd047d4756b26d829c62
Holt Picklesimer <holtpicklesimer@gmail.com>
2020-11-14 12:46:20 -0700

    Added Moon orbit and rotation.
    Added Moon orbit and rotation.

---
+++ Artemis/Assets/Scripts/Orbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform center;
+    public Vector3 axis = Vector3.up;
+    public float radius = 2f;
+    public float orbitSpeed = 45f;
+    public float rotateSpeed = 90f;
+
+    void Start()
+    {
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+    }
+
+    void Update()
+    {
+        OrbitMovement();
+    }
+
+    void OrbitMovement()
+    {
+        var initRotation = transform.rotation * Quaternion.Euler(0, rotateSpeed * Time.deltaTime, 0);
+        transform.RotateAround(center.position, axis, orbitSpeed * Time.deltaTime);
+        transform.rotation = initRotation;
+    }
+}
