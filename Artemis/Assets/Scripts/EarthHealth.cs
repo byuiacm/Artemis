7d584cac6fcc032710e78a9b92be1987721d5c0e
JackKit <chillkit@users.noreply.github.com>
2020-11-15 23:14:38 -0700

    Created a bottom Health Bar and the earth health script, when pressing space. Its health will reduce.

---
+++ Artemis/Assets/Scripts/EarthHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.setMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            currentHealth -= 10;
+        }
+
+        healthbar.setHealth(currentHealth);
+    }
+}
