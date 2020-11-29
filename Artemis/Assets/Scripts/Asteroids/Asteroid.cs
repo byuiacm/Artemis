28d914b7ea126607fffe656027b2d8f60549b2dd
Jonathan Rivera <johny24rivera@outlook.com>
2020-11-28 20:14:37 -0700

    adding new scripts

---
+++ Artemis/Assets/Scripts/Asteroids/Asteroid.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float changeX;
+    private float changeY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        getVelocity();
+    }
+
+    private void getVelocity()
+    {   
+        // In game camera is looking at x and z axis (z is y in this case)
+        float x = transform.position.x;
+        float y = transform.position.z;
+
+        changeX = -x / 1200;
+        changeY = -y / 1200;
+
+    }
+
+
+    public void kill()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+        float y = transform.position.z;
+        transform.position = new Vector3(x + changeX, 0, y + changeY);
+
+        if (transform.position.x < 10 && transform.position.x > -10)
+        {
+            //Change the if statement
+            kill();
+        }
+    }
+}
---
+++ Artemis/Assets/Scripts/Asteroids/deployAsteroids.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployAsteroids : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float respawnTime = 2.0f;
+    private Vector3 screenBounds;
+    private int count = 0;
+    List<GameObject> asteroids;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, Screen.height));
+        StartCoroutine(asteroidWave());
+        asteroids = new List<GameObject>();
+    }
+
+    private Vector3 getSpawnLocation()
+    {
+        Vector3 startPosition;
+        count++;
+        int condition = count % 4;
+        
+        
+      switch (condition)
+      {
+            case 1:
+                startPosition = new Vector3(Random.Range(-screenBounds.x, screenBounds.x), 0 , -screenBounds.z);
+                break;
+            case 2:
+                startPosition = new Vector3(screenBounds.x, 0,Random.Range(-screenBounds.z, screenBounds.z));
+                break;
+            case 3:
+                startPosition = new Vector3(-screenBounds.x, 0, Random.Range(-screenBounds.z, screenBounds.z));
+                break;
+            default:
+                startPosition = new Vector3(Random.Range(-screenBounds.x, screenBounds.x), 0, screenBounds.z);
+                break;
+      }
+
+        return startPosition;
+    }
+
+    private void spawnEnemy()
+    { 
+        if (asteroids.Count < 5)
+        {
+            GameObject a = Instantiate(asteroidPrefab) as GameObject;
+            asteroids.Add(a);
+            a.transform.position = getSpawnLocation();
+        }
+    }
+
+    private void checkAlive()
+    {
+        var toRemove = new List<GameObject>();
+        foreach (GameObject p in asteroids)
+        {
+            if (p == null)
+            {
+                toRemove.Add(p);
+            }
+        }
+
+        foreach (GameObject p in toRemove)
+        {
+            asteroids.Remove(p);
+        }
+
+    }
+
+    // Update is called once per frame
+    IEnumerator asteroidWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+            checkAlive();
+        }
+    }
+}
